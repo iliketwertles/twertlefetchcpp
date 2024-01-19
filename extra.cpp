bf2aa501defd45ec164e6802d2d6d6a096eb90f4
iliketwertles <jarodjoe1210@gmail.com>
2024-01-18 23:49:00 -0500

    moved to classes, looks better

--- extra.cpp
+++ extra.cpp
@@ -7,8 +7,8 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
 #include <sys/utsname.h>
-
 
 struct utsname unameData;
 
@@ -32,75 +32,90 @@ std::string exec(const char* cmd) {
     return result;
 }
 
+class SystemInfoExt {
+public:
+    const char * arch() const {
+        //getting arch
+        uname(&unameData);
+        const char * arch = unameData.machine;
+        return arch;
+    }
 
-void fumo(std::string distro, std::string kernel, std::string packages, std::string desktop, double diff) {
+    const std::string cpu() const {
+        //getting cpu info
+        std::string cpuinfo_s, cpu, line, init_name;
+        std::ifstream cpuinfo ("/proc/cpuinfo");
+        if ( cpuinfo.is_open() ) {
+            std::string model = "model name";
+            while ( std::getline(cpuinfo, line) ) {
+                size_t found = line.find(model);
+                if (found != std::string::npos) {
+                    cpu = line.substr(found + 13);
 
-    // getting arch
-    uname(&unameData);
-    std::string arch = unameData.machine;
+                    cpu.erase(0, cpu.find_first_not_of(" \t"));
+                    cpu.erase(cpu.find_last_not_of(" \t") + 1);
+                    line = "";
 
-    //getting cpu info
-    std::string cpuinfo_s, cpu, line, init_name;
-    std::ifstream cpuinfo ("/proc/cpuinfo");
-    if ( cpuinfo.is_open() ) {
-        std::string model = "model name";
-        while ( std::getline(cpuinfo, line) ) {
-            size_t found = line.find(model);
-            if (found != std::string::npos) {
-                cpu = line.substr(found + 13);
-
-                cpu.erase(0, cpu.find_first_not_of(" \t"));
-                cpu.erase(cpu.find_last_not_of(" \t") + 1);
-                line = "";
-
-                break;
+                    break;
+                }
             }
+        } else {
+            std::cout << "/proc/cpuinfo isnt open!" << std::endl;
         }
-    } else {
-        std::cout << "/proc/cpuinfo isnt open!" << std::endl;
+        return cpu;
     }
 
-    //getting gpu (please help me)
-    //std::string gpu = exec("lspci | grep -m 1 ' VGA ' | cut -d" " -f 9-")
-
-    //getting init
-    if ( std::filesystem::exists("/etc/systemd/system.conf") ) {
-        init_name = "Systemd";
-    } else if ( std::filesystem::exists("/etc/init.d") ) {
-        init_name = "OpenRC";
-    } else if ( std::filesystem::exists("/etc/runit/1") ) {
-        init_name = "Runit";
-    } else {
-        init_name = "i dunno";
+    const std::string gpu() const {
+        return trim(exec("lspci | grep -m 1 ' VGA ' | cut -d\" \" -f 9-"));
     }
 
-    //getting meminfo
-    std::ifstream meminfo ("/proc/meminfo");
-    std::string total;
-    std::string available;
-    if ( meminfo.is_open() ) {
-        total = "MemTotal:";
-        available = "MemAvailable:";
-        while ( std::getline(meminfo, line) ) {
-            size_t foundt = line.find(total);
-            size_t founda = line.find(available);
-            if ( foundt != std::string::npos ) {
-                total = line.substr(foundt + total.length());
-                total.erase(0, total.find_first_not_of(" \t"));
-                total.erase(total.find_last_not_of(" \t") + 1);
-                total = total.substr(0, total.size()-3);
-            } else if ( founda != std::string::npos ) {
-                available = line.substr(founda + available.length());
-                available.erase(0, available.find_first_not_of(" \t"));
-                available.erase(available.find_last_not_of(" \t") + 1);
-                available = available.substr(0, available.size()-3);
-            }
-            if ( total != "MemTotal:" && available != "MemAvailable:" ) { break; }
+    const char * init() const {
+        if ( std::filesystem::exists("/etc/systemd/system.conf") ) {
+            return "Systemd";
+        } else if ( std::filesystem::exists("/etc/init.d") ) {
+            return "OpenRC";
+        } else if ( std::filesystem::exists("/etc/runit/1") ) {
+            return "Runit";
+        } else {
+            return "i dunno";
         }
     }
-    meminfo.close();
-    int used = std::stoi(total) - std::stoi(available);
 
+    // total = mem[0]
+    // used = mem[1]
+    const std::vector<int> mem() const {
+        std::ifstream meminfo ("/proc/meminfo");
+        std::string total, available, line;
+        std::vector<int> ret; 
+        if ( meminfo.is_open() ) {
+            total = "MemTotal:";
+            available = "MemAvailable:";
+            while ( std::getline(meminfo, line) ) {
+                size_t foundt = line.find(total);
+                size_t founda = line.find(available);
+                if ( foundt != std::string::npos ) {
+                    total = line.substr(foundt + total.length());
+                    total.erase(0, total.find_first_not_of(" \t"));
+                    total.erase(total.find_last_not_of(" \t") + 1);
+                    total = total.substr(0, total.size()-3);
+                } else if ( founda != std::string::npos ) {
+                    available = line.substr(founda + available.length());
+                    available.erase(0, available.find_first_not_of(" \t"));
+                    available.erase(available.find_last_not_of(" \t") + 1);
+                    available = available.substr(0, available.size()-3);
+                }
+                if ( total != "MemTotal:" && available != "MemAvailable:" ) { break; }
+            }
+        }
+        ret.push_back(std::stoi(total));
+        ret.push_back(std::stoi(total) - std::stoi(available));
+        meminfo.close();
+        return ret;
+    }
+     
+};
+
+void fumo(std::string distro, std::string kernel, int packages, std::string desktop, double diff) {
 
     printf("⠀⢀⣒⠒⠆⠤⣀⡀⠀\n");
     printf("⢠⡛⠛⠻⣷⣶⣦⣬⣕⡒⠤⢀⣀⠀\n");
@@ -111,14 +126,14 @@ void fumo(std::string distro, std::string kernel, std::string packages, std::str
     printf("⠀⠀⠹⣿⣿⣵⣿⠃⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⡄\n");
     printf("⠀⠀⠀⠈⠛⣯⡇⠛⣽⣦⣿⠀⠀⠀⠀⢀⠔⠙⣄⠀⠀⠀⠀⠀⠀⣠⠳⡀⠀⠀⠀⠀⢿⡵⡀⠀⠀       Distro:\033[36m %s \033[0m \n", distro.c_str());
     printf("⠀⠀⠀⠀⣸⣿⣿⣿⠿⢿⠟⠀⠀⠀⢀⡏⠀⠀⠘⡄⠀⠀⠀⠀⢠⠃⠀⠹⡄⠀⠀⠀⠸⣿⣷⡀⠀⠀      Kernel:\033[33m %s \033[0m \n", kernel.c_str());
-    printf("⠀⠀⠀⠸⣿⣿⠟⢹⣥⠀⠀⠀⠀⠀⣸⣀⣀⣤⣀⣀⠈⠳⢤⡀⡇⣀⣠⣄⣸⡆⠀⠀⠀⡏⠀⠀        Packages:\033[34m %s \033[0m \n", packages.c_str());
+    printf("⠀⠀⠀⠸⣿⣿⠟⢹⣥⠀⠀⠀⠀⠀⣸⣀⣀⣤⣀⣀⠈⠳⢤⡀⡇⣀⣠⣄⣸⡆⠀⠀⠀⡏⠀⠀        Packages:\033[34m %i \033[0m \n", packages);
     printf("⠀⠀⠀⠀⠁⠁⠀⢸⢟⡄⠀⠀⠀⠀⣿⣾⣿⣿⣿⣿⠁⠀⠈⠙⠙⣯⣿⣿⣿⡇⠀⠀⢠⠃          Desktop:\033[31m %s \033[0m \n", desktop.c_str());
-    printf("⠀⠀⠀⠀⠀⠀⠀⠇⢨⢞⢆⠀⠀⠀⡿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⣿⣿⣿⡿⡇⠀⣠⢟⡄          Arch:\033[36m %s \033[0m \n", arch.c_str());
-    printf("⠀⠀⠀⠀⠀⠀⡼⠀⢈⡏⢎⠳⣄⠀⡇⠙⠛⠟⠛⠀⠀⠀⠀⠀⠀⠘⠻⠛⢱⢃⡜⡝⠈⠚⡄         CPU:\033[33m %s \033[0m \n", cpu.c_str());
-    printf("⠀⠀⠀⠀⠀⠘⣅⠁⢸⣋⠈⢣⡈⢷⠇⠀⠀⠀⠀⠀⣄⠀⠀⢀⡄⠀⠀⣠⣼⢯⣴⠇⣀⡀⢸         GPU:\033[34m %s \033[0m \n", trim(exec("lspci | grep -m 1 ' VGA ' | cut -d\" \" -f 9-")).c_str());
-    printf("⠀⠀⠀⠀⠀⠀⠈⠳⡌⠛⣶⣆⣷⣿⣦⣄⣀⠀⠀⠀⠈⠉⠉⢉⣀⣤⡞⢛⣄⡀⢀⡨⢗⡦⠎         Init:\033[31m %s \033[0m \n", init_name.c_str());
+    printf("⠀⠀⠀⠀⠀⠀⠀⠇⢨⢞⢆⠀⠀⠀⡿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⣿⣿⣿⡿⡇⠀⣠⢟⡄          Arch:\033[36m %s \033[0m \n", SystemInfoExt().arch());
+    printf("⠀⠀⠀⠀⠀⠀⡼⠀⢈⡏⢎⠳⣄⠀⡇⠙⠛⠟⠛⠀⠀⠀⠀⠀⠀⠘⠻⠛⢱⢃⡜⡝⠈⠚⡄         CPU:\033[33m %s \033[0m \n", SystemInfoExt().cpu().c_str());
+    printf("⠀⠀⠀⠀⠀⠘⣅⠁⢸⣋⠈⢣⡈⢷⠇⠀⠀⠀⠀⠀⣄⠀⠀⢀⡄⠀⠀⣠⣼⢯⣴⠇⣀⡀⢸         GPU:\033[34m %s \033[0m \n", SystemInfoExt().gpu().c_str());
+    printf("⠀⠀⠀⠀⠀⠀⠈⠳⡌⠛⣶⣆⣷⣿⣦⣄⣀⠀⠀⠀⠈⠉⠉⢉⣀⣤⡞⢛⣄⡀⢀⡨⢗⡦⠎         Init:\033[31m %s \033[0m \n", SystemInfoExt().init());
     printf("⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠪⣿⠁⠀⠐⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠉⠁⢸⠀⠀⠀⠄⠙⡆         Shell:\033[36m %s \033[0m \n", getenv("SHELL"));
-    printf("⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠚⡉⢳⡄⠡⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠁⣠⣧⣤⣄⣀⡀⡰⠁         Mem:\033[33m %i/%imb \033[0m \n", used / 1024, std::stoi(total) / 1024);
+    printf("⠀⠀⠀⠀⠀⠀⠀⠀⣀⠤⠚⡉⢳⡄⠡⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣏⠁⣠⣧⣤⣄⣀⡀⡰⠁         Mem:\033[33m %i/%imb \033[0m \n", SystemInfoExt().mem()[1] / 1024, SystemInfoExt().mem()[0] / 1024);
     printf("⠀⠀⠀⠀⠀⢀⠔⠉⠀⠀⠀⠀⢀⣧⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣅⡀\n");
     printf("⠀⠀⠀⠀⠀⢸⠆⠀⠀⠀⣀⣼⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⠋⠁⣠⠖⠒⠒⠛⢿⣆\n");
     printf("⠀⠀⠀⠀⠀⠀⠑⠤⠴⠞⢋⣵⣿⢿⣿⣿⣿⣿⣿⣿⠗⣀⠀⠀⠀⠀⠀⢰⠇⠀⠀⠀⠀⢀⡼⣶⣤\n");
--- main.cpp
+++ main.cpp
@@ -10,89 +10,89 @@
 
 struct stat fileInfo;
 
-int main(int argc, char* argv[]) {
-    namespace fs = std::filesystem;
-    // defining
-    const time_t today = std::time(0);
-    std::string distro, kernel, packages, desktop, osrelease_s, line;
-    time_t birth_date;
-    std::ifstream osrelease ("/etc/os-release");
-
-    // reading /etc/os-release to get distro name, under NAME=
-    if ( osrelease.is_open() ) {
-        std::string name = "NAME=";
-        while ( std::getline(osrelease, line) ) {
-            size_t found = line.find(name);
-            if ( found != std::string::npos ) {
-                distro = line.substr(found + name.length());
-
-                distro.erase(0, distro.find_first_not_of(" \t"));
-                distro.erase(distro.find_last_not_of(" \t") + 1);
-
-                break;
-            }
-        }
-    } else {
-        std::cout << "/etc/os-release isnt open!" << std::endl;
-        return 1;
-    }
-    osrelease.close();
-    
-    // getting kernel version
-    uname(&unameData);
-    kernel = unameData.release;
-    
-    // getting packages, only works for emerge from gentoo
-    if (fs::is_directory("/var/db/pkg")) {
-        int count = 0;
-        for (const auto& entry : fs::directory_iterator("/var/db/pkg")) {
-            if (fs::is_directory(entry.path())) {
-                for (const auto& sub_entry : fs::directory_iterator(entry.path())) {
-                    if (fs::is_directory(sub_entry.path())) {
-                        count++;
+class SystemInfo {
+    public:
+        const std::string distro() const {
+            //getting distro name
+            std::string distro_s, line;
+            std::ifstream osrelease ("/etc/os-release");
+            if ( osrelease.is_open() ) {
+                std::string name = "NAME=";
+                while ( std::getline(osrelease, line) ) {
+                    size_t found = line.find(name);
+                    if ( found != std::string::npos ) {
+                        distro_s = line.substr(found + name.length());
+                        distro_s.erase(0, distro_s.find_first_not_of(" \t"));
+                        distro_s.erase(distro_s.find_last_not_of(" \t") + 1);
+                        break;
                     }
                 }
+            } else {
+                distro_s = "Unknown";
+            }
+            osrelease.close();
+            return distro_s;
+        }
+
+        const char * kernel() const {
+            uname(&unameData);
+            return unameData.release;
+        }
+
+        const int packages() const {
+            // getting packages, only works for emerge from gentoo
+            if (std::filesystem::is_directory("/var/db/pkg")) {
+                int count = 0;
+                for (const auto& entry : std::filesystem::directory_iterator("/var/db/pkg")) {
+                    if (std::filesystem::is_directory(entry.path())) {
+                        for (const auto& sub_entry : std::filesystem::directory_iterator(entry.path())) {
+                            if (std::filesystem::is_directory(sub_entry.path())) {
+                                count++;
+                            }
+                        }
+                    }
+                }
+                return count;
+            } else {
+                return 0;
             }
         }
-        packages = std::to_string(count);
-    } else {
-        packages = "unsupported :(";
-    }
 
-    // getting desktop env, if it cant read current desktop
-    // it reads desktop session, if both are null then you
-    // dont have a desktop
-    if ( getenv("XDG_CURRENT_DESKTOP") != NULL ) {
-        desktop = getenv("XDG_CURRENT_DESKTOP");
-    } else if ( getenv("DESKTOP_SESSION") != NULL ) {
-        desktop = getenv("DESKTOP_SESSION");
-    } else {
-        desktop = "i dunno :(";
-    }
+        const char * desktop() const {
+            if ( getenv("XDG_CURRENT_DESKTOP") != NULL ) {
+                return getenv("XDG_CURRENT_DESKTOP");
+            } else if ( getenv("DESKTOP_SESSION") != NULL ) {
+                return getenv("DESKTOP_SESSION");
+            } else {
+                return "i dunno :(";
+            }
+        }
 
-    // getting date of linux install (theoretical)
-    // uses metadata of the path / to simulate the 
-    // oldest thing, obv doesnt work  if you 
-    // reformatted root
-    if ( stat("/", &fileInfo) == 0 ) {
-        birth_date = fileInfo.st_ctime;
-    } else {
-        birth_date = 0;
-    }
-    double diff = std::difftime(today, birth_date) / (60 * 60 * 24);
+        const int DoB() const {
+            const time_t today = std::time(0);
+            time_t birth_date;
+            if ( stat("/", &fileInfo) == 0 ) {
+                birth_date = fileInfo.st_ctime;
+            } else {
+                birth_date = 0;
+            }
+            return std::difftime(today, birth_date) / (60 * 60 * 24);
+        }
+};
 
+int main(int argc, char* argv[]) {
     // printing everything out, idk a better way to do it 
     if (argc >= 2 && strcmp(argv[1], "-f") == 0) {
-        fumo(distro, kernel, packages, desktop, diff);
+        fumo(SystemInfo().distro(), SystemInfo().kernel(), SystemInfo().packages(), SystemInfo().desktop(), SystemInfo().DoB());
         return 0;
     } else {
         std::cout << "     .--." << '\n';
-        std::cout << "    |o_o |    Distro: " << "\033[36m" << distro <<  "\033[0m" << '\n';
-        std::cout << "    |:_/ |    Kernel: " << "\033[33m" << kernel <<  "\033[0m" << '\n';
-        std::cout << R"(   //   \ \   Packages: )" << "\033[34m" << packages <<  "\033[0m" << '\n';
-        std::cout << "  (|     | )  Desktop: " << "\033[31m" << desktop <<  "\033[0m" << '\n';
+        std::cout << "    |o_o |    Distro: " << "\033[36m" << SystemInfo().distro() <<  "\033[0m" << '\n';
+        std::cout << "    |:_/ |    Kernel: " << "\033[33m" << SystemInfo().kernel() <<  "\033[0m" << '\n';
+        std::cout << R"(   //   \ \   Packages: )" << "\033[34m" << SystemInfo().packages() <<  "\033[0m" << '\n';
+        std::cout << "  (|     | )  Desktop: " << "\033[31m" << SystemInfo().desktop() <<  "\033[0m" << '\n';
         std::cout << R"( /'\_   _/`\ )" << '\n';
-        std::cout << R"( \___)=(___/  You installed )" << (int) diff << " days ago" << '\n';
+        std::cout << R"( \___)=(___/  You installed )" << SystemInfo().DoB() << " days ago" << '\n';
     }
     return 0;
 }
